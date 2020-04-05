008e0471758bce8711683e70315c0c9bf30949d2
Fin Christensen <christensen.fin@gmail.com>
2020-04-05 21:41:56 +0200

    Fix broken build step for new translation files

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -5,5 +5,6 @@ set -e
 for po in locale/*.po
 do
     lang="${po%.po}"
+    mkdir -p "${lang/_??/}/LC_MESSAGES"
     "${MSGFMT}" --output "${lang/_??/}/LC_MESSAGES/gnome-shell-extension-cpupower.mo" "$po"
 done
